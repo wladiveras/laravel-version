e05adc76248563c5a4802f913d6cb1e2e14d70b3
Wladi Veras <wladinart@gmail.com>
2024-02-09 23:20:15 -0300

    fix: :bug: change city_id to cities

--- database/migrations/2024_02_10_021125_create_addresses_table.php
+++ database/migrations/2024_02_10_021125_create_addresses_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('city_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
         });
     }
 
